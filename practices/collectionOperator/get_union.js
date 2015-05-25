ed7c26a7ffa10499e61888e3196519504d7380b6
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-25 14:19:45 +0800

    get_union finish

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -4,9 +4,8 @@ function get_union(collection_a, collection_b) {
   //在这里写入代码
   //在第一个数组元素中删除与第二个数组相同的元素
   var _ = require('../lodash/array');
-  var temp = [];
-  temp = _.difference(collection_b,collection_a);
-  return collection_a.concat(temp);
+
+  return _.union(collection_a, collection_b);
 }
 
 module.exports = get_union;
