4bb0653d99d1e5221e202edc7da0b73f7f01a580
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-25 12:13:35 +0800

    first commit

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,14 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var _ = require('../lodash/array');
+  var temp = [];
+  for (var i = 0; i < collection.length; i++) {
+    if(collection[i] %2 != 0){
+      temp.push(collection[i]);
+    }
+  }
+  return _.difference(collection,temp);
 }
 
 module.exports = collect_all_even;
