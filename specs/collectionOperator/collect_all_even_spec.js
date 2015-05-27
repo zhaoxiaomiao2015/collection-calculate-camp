7c78bd77705bb8da8ab4727a5549df41518aed4d
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-27 09:41:59 +0800

    first commit

--- specs/collectionOperator/collect_all_even_spec.js
+++ specs/collectionOperator/collect_all_even_spec.js
@@ -4,6 +4,7 @@ var collect_all_even = require('../../practices/collectionOperator/collect_all_e
 
 describe('collect_all_even', function () {
 
+
   var collection_a = [1, 2, 3, 4, 5];
   var collection_b = [2, 4];
 
@@ -13,4 +14,3 @@ describe('collect_all_even', function () {
     expect(result).toEqual(collection_b);
   });
 });
-
