67cc39134f7d9e4fb93962857deb8d90f95a9e1b
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-27 13:10:35 +0800

    choose_even.js finish

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,12 +1,12 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //在这里写入代码
-  var _ = require('../lodash/array');
-  var evens = _.remove(collection, function(n) {
-    return n % 2 === 0;
-    });
+  var evens = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 ===0) {
+      evens.push(collection[i]);
+    }
+  }
   return evens;
 }
 
