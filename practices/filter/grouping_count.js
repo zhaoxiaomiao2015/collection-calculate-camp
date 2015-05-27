8aec324ba2e74f797bd6569018c37164241f17ff
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-27 16:21:58 +0800

    grouping_count.js finish

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  var result = {};
+  for (var i = 0; i < collection.length; i++) {
+    var count = 0;
+    if (!result[collection[i]]) {
+      result[collection[i]] = 0;
+    }
+    if (result[collection[i]]) {
+      count = result[collection[i]];
+    }
+      result[collection[i]] = count + 1;
+  }
+  return result;
 }
 
 module.exports = grouping_count;
