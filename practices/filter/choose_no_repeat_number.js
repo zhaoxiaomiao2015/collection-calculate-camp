2dbd308dc3d2e1ae1e6de4e9abbb6a61567e8023
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-27 14:09:57 +0800

    choose_no_repeat_number.js

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,10 +1,19 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
-  var _ = require('../lodash/array');
-  return _.uniq(collection);
-}
+  var temp = [];
+  for (var i = 0; i < collection.length; i++) {
+    var flag = false;
+    for (var x = 0; x < temp.length; x++) {
+      if (temp[x] === collection[i]) {
+        flag = true;
+      }
+    }
+    if (!flag) {
+      temp.push(collection[i]);
+      }
+    }
+      return temp;
+  }
 
 module.exports = choose_no_repeat_number;
