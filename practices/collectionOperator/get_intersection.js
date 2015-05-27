b71f5a390f2156efae26b83ac58bb7b8de79f314
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-27 10:09:08 +0800

    get_intersection.jsp finish

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,8 +2,15 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
-  var _ = require('../lodash/array');
-  return _.intersection(collection_b,collection_a);
+  var collection_c = [];
+  for (var i = 0; i < collection_b.length; i++) {
+    for (var x = 0; x < collection_a.length; x++) {
+      if (collection_a[x] === collection_b[i]) {
+        collection_c.push(collection_b[i]);
+      }
+    }
+  }
+  return collection_c;
 }
 
 module.exports = get_intersection;
