d2f4ddca1a0349d8cdb644e8a72ed0b427a0a2fe
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-27 13:12:45 +0800

    choose_multiples.jsp finish

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,12 +1,12 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //在这里写入代码
-  var _ = require('../lodash/array');
-  var evens = _.remove(collection, function(n) {
-    return n % 3 === 0;
-    });
+  var evens = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] % 3 ===0) {
+      evens.push(collection[i]);
+    }
+  }
   return evens;
 }
 
