b4989b56e24ca5c2f3934d4d1d0f4526c72ff035
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-27 13:01:26 +0800

    get_union.js finish

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
-  //在第一个数组元素中删除与第二个数组相同的元素
-  var _ = require('../lodash/array');
+  var union = collection_a;
+  var differedce_element = collection_b;
+  for (var i = 0; i < collection_a.length; i++) {
+    for (var x = 0; x < collection_b.length; x++) {
+        if (collection_b[x] == collection_a[i]) {
+          differedce_element.splice(x,1);
+        }
+      }
+    }
 
-  return _.union(collection_a, collection_b);
-}
+  for (var y = 0; y < differedce_element.length; y++) {
+    union.push(differedce_element[y]);
+    }
+    return union;
+  }
+
 
 module.exports = get_union;
