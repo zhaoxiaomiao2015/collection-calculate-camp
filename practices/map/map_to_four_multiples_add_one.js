7b60f171e4f5d7629cefc4febcbe7977cb39b6c6
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-28 20:55:14 +0800

    map to finish

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,13 +1,12 @@
 'use strict';
 
-function double(element){
-  var  result = 4 * element + 1;
-  return result;
-}
 var map_to_four_multiples_add_one = function(collection){
+  var double_collection = [];
+  for (var i = 0; i < collection.length; i++) {
+      double_collection.push(collection[i] * 4 + 1);
+  }
+  return double_collection;
 
-  var _ = require('../lodash/array');
-  return _.map(collection, double);
 };
 
 module.exports = map_to_four_multiples_add_one;
