886ce179cd81722393d6dcdb7e36168483e1689c
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-28 11:39:06 +0800

    two_collections_practice_3.js finish

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+    var temp = [];
+    for (var i = 0; i < collection_a.length; i++) {
+        for (var x = 0; x < collection_b.length; x++) {
+            var one = collection_a[i];
+            var two = collection_b[x];
+            if (one % two === 0) {
+                temp.push(one);
+                break;
+            }
+        }
+    }
+    return temp;
 }
 
 module.exports = choose_divisible_integer;
