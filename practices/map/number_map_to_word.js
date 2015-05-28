ccf758ce063cbee9187ee74daa84ce4f37a71d59
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-28 21:31:31 +0800

    number word finish

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,10 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+    var collection_b = [];
+    for (var i = 0; i < collection.length; i++) {
+      collection_b.push(String.fromCharCode(collection[i] + 96));
+    }
+    return collection_b;
 };
 
 module.exports = number_map_to_word;
